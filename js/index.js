edf55aa32af148de51050322c9066016b8b97883
Vitalii-Prokopenko <doc8259@gmail.com>
2023-01-22 09:36:11 +0200

    update 22/01/2023

--- js/index.js
+++ js/index.js
@@ -445,3 +445,39 @@
 //   default:
 //     console.log("The number is not primary");
 // }
+
+// ========================= CODEWARS ==========================
+// Digital root is the recursive sum of all the digits in a number.
+
+// Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing in this way until a single-digit number is produced. The input will be a non-negative integer.
+
+// Examples
+//     16  -->  1 + 6 = 7
+//    942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
+// 132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
+// 493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->
+
+function digitalRoot(n) {
+  let levelOfExponention = 1;
+  let digitReminder = 0;
+  let numberOfDigits = 1;
+  let numberWithoutFoundDigits = n;
+  let digitArray = [];
+  do {
+    numberOfDigits = 10 ** levelOfExponention;
+    digitReminder = numberWithoutFoundDigits % numberOfDigits;
+    digitArray.push(digitReminder / 10 ** (levelOfExponention - 1));
+    console.log(digitArray);
+    numberWithoutFoundDigits -= digitReminder;
+    levelOfExponention += 1;
+  } while (n > numberOfDigits);
+  let sum = 0;
+  for (let q = 0; q < digitArray.length; q += 1) {
+    let digit = digitArray[q];
+    sum += digit;
+  }
+  return sum;
+}
+
+const result = digitalRoot(16);
+console.log(result);
