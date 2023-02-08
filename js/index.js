7251a880ef9bbe41d8a033d44c5c9b03363e56ce
Vitalii-Prokopenko <doc8259@gmail.com>
2023-02-08 21:37:08 +0200

    update 08/02/2023

--- js/index.js
+++ js/index.js
@@ -852,20 +852,83 @@ const forecast = {
 
 // console.log(menu);
   
-const salaries = {
-  Mango: 570,
-  Poly: 900,
-  Ajax: 1470,
+// const salaries = {
+//   Mango: 570,
+//   Poly: 900,
+//   Ajax: 1470,
+// }
+
+// const totalSalary = (salaries) => {
+//   let sum = 0;
+//   const values = Object.values(salaries);
+//   console.log(values);
+//   for (const value of values) {
+//     sum += value;
+//     console.log(sum);
+//   }
+//   return sum;
+// };
+// console.log(totalSalary(salaries));
+
+// // 
+// class StringBuilder {
+//   constructor(initialValue) {
+//     this.value = initialValue;
+//   };
+//   getValue() {
+//     return this.value;
+//   };
+//   padEnd(str) {
+//     this.value += str;    
+//   };
+//   padStart(str) {
+//     this.value = str + this.value;
+//   };
+//   padBoth(str) {
+//     this.value = str + this.value + str;
+//   }
+// }
+
+
+// // Change code above this line
+// const builder = new StringBuilder(".");
+// console.log(builder.getValue()); // "."
+
+// builder.padStart("^");
+// console.log(builder.getValue()); // "^."
+// builder.padEnd("^");
+// console.log(builder.getValue()); // "^.^"
+// builder.padBoth("=");
+// console.log(builder.getValue()); // "=^.^="
+
+
+class Car {
+  // Change code below this line
+  static MAX_PRICE = 50000;
+  #price;
+
+  constructor({ price }) {
+    this.#price = price;
+  }
+
+  get price() {
+    return this.#price;
+  }
+
+  set price(newPrice) {
+    if (newPrice <= Car.MAX_PRICE) {
+      this.#price = newPrice;
+    };
+    
+  }
+  // Change code above this line
 }
 
-const totalSalary = (salaries) => {
-  let sum = 0;
-  const values = Object.values(salaries);
-  console.log(values);
-  for (const value of values) {
-    sum += value;
-    console.log(sum);
-  }
-  return sum;
-};
-console.log(totalSalary(salaries));
+const audi = new Car({ price: 35000 });
+console.log(audi.price); // 35000
+
+audi.price = 49000;
+console.log(audi.price); // 49000
+
+audi.price = 51000;
+console.log(audi.price); // 49000
