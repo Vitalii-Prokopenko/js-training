c39f3cf8edaba7da410f00c9f027acf1b98f3710
Vitalii-Prokopenko <doc8259@gmail.com>
2023-02-09 22:21:27 +0200

    update 09/02/2023

--- js/index.js
+++ js/index.js
@@ -902,33 +902,85 @@ const forecast = {
 // console.log(builder.getValue()); // "=^.^="
 
 
-class Car {
-  // Change code below this line
-  static MAX_PRICE = 50000;
-  #price;
+// class Car {
+//   // Change code below this line
+//   static MAX_PRICE = 50000;
+//   #price;
 
-  constructor({ price }) {
-    this.#price = price;
-  }
+//   constructor({ price }) {
+//     this.#price = price;
+//   }
 
-  get price() {
-    return this.#price;
-  }
+//   get price() {
+//     return this.#price;
+//   }
 
-  set price(newPrice) {
-    if (newPrice <= Car.MAX_PRICE) {
-      this.#price = newPrice;
-    };
+//   set price(newPrice) {
+//     if (newPrice <= Car.MAX_PRICE) {
+//       this.#price = newPrice;
+//     };
     
+//   }
+//   // Change code above this line
+// }
+
+// const audi = new Car({ price: 35000 });
+// console.log(audi.price); // 35000
+
+// audi.price = 49000;
+// console.log(audi.price); // 49000
+
+// audi.price = 51000;
+// console.log(audi.price); // 49000
+class User {
+  email;
+
+  constructor(email) {
+    this.email = email;
   }
+
+  get email() {
+    return this.email;
+  }
+
+  set email(newEmail) {
+    this.email = newEmail;
+  }
+}
+class Admin extends User {
+  // Change code below this line
+
+  static AccessLevel = {
+    BASIC: "basic",
+    SUPERUSER: "superuser",
+  };
+
+  constructor({ email, accessLevel }) {
+    super(email);
+    this.accessLevel = accessLevel;
+    this.blacklistedEmails = [];
+
+    blacklist(email) {
+      this.blacklistedEmails.push(email);
+    };
+
+    isBlacklisted(email) {
+      return this.blacklistedEmails.includes(email);
+    };
+  };
+
   // Change code above this line
 }
 
-const audi = new Car({ price: 35000 });
-console.log(audi.price); // 35000
+const mango = new Admin({
+  email: "mango@mail.com",
+  accessLevel: Admin.AccessLevel.SUPERUSER,
+});
 
-audi.price = 49000;
-console.log(audi.price); // 49000
+console.log(mango.email); // "mango@mail.com"
+console.log(mango.accessLevel); // "superuser"
 
-audi.price = 51000;
-console.log(audi.price); // 49000
+mango.blacklist("poly@mail.com");
+console.log(mango.blacklistedEmails); // ["poly@mail.com"]
+console.log(mango.isBlacklisted("mango@mail.com")); // false
+console.log(mango.isBlacklisted("poly@mail.com")); // true
